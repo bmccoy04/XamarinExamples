daced9bf48746a40857650d73430189b1759bb37
bmccoy04 <bryanmccoy04@gmail.com>
2020-01-26 14:23:02 -0600

    Add project files.

---
+++ XamarinExamples/XamarinExamples/MenuItems/MasterPageItems.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XamarinExamples.MenuItems
+{
+    public class MasterPageItems
+    {
+        public string Title { get; set; }
+        public string Icon { get; set; }
+        public Type TargetType { get; set; }
+    }
+}
---
+++ XamarinExamples/XamarinExamples/Views/MenuPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XamarinExamples.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MenuPage : ContentPage
+    {
+        public MenuPage()
+        {
+            InitializeComponent();
+
+            var menuItems = new List<MenuPageItem> { new MenuPageItem() { Name = "About" } };
+
+            ListViewMenu.ItemsSource = menuItems;
+
+        }
+    }
+
+    public class MenuPageItem
+    {
+        public string Name { get; set; }
+    }
+}
